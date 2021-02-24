666841ce2488110a64b929535bb49b45d7f8384f
Abirk2day <abirk2day@gmail.com>
2021-02-24 22:59:42 +0600

    react

--- ecommerce-websit/src/App.js
+++ ecommerce-websit/src/App.js
@@ -1,12 +1,45 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-     
+      <h1>I am react</h1>
+      <Users></Users>
+
+ 
     </div>
   );
+}
+
+
+
+
+function Users () {
+ const [users, setUser] = useState([]);
+ useEffect(() =>{
+   fetch('https://jsonplaceholder.typicode.com/users')
+   .then(res => res.json())
+   .then(data => setUser(data))
+   console.log(setUser)
+ }, [])
+
+
+ 
+
+
+  return(
+    <div>
+      <h1>dynamic user: {users.length}</h1>
+     {
+      users.map(user => <div className="userInterface">Name:{user.name} :: User Name:{user.username} Email:{user.email}  <button onClick="handelUserView()">View details</button></div>)
+     }
+
+    </div>
+  )
 }
 
 export default App;
